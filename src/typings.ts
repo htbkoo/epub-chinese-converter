c33bf3e3b2fce4acf34806d9668fff55e3b6f89b
htbkoo <h1017760@connect.hku.hk>
2020-02-23 12:43:07 +0800

    updated typings

--- src/typings.ts
+++ src/typings.ts
@@ -1,7 +1,7 @@
 import {Metadata as EpubMetadata} from "epub";
 
 export namespace Book {
-    export type Metadata = EpubMetadata;
+    export type Metadata = EpubMetadata | object;
     export type ChapterText = string;
     export type Chapter = object & { text: ChapterText };
     export type Chapters = { [id: string]: Chapter };
